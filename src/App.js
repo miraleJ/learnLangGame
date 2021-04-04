91cca6a736a1ec327f7d1fab3f21bf7f973f7e37
miraleJ <miralek@gmail.com>
2021-04-04 23:59:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn First Words In English.
         </a>
       </header>
     </div>
