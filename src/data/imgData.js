e44e8746d6529f3896d68d600a8b50d7f986e378
miraleJ <miralek@gmail.com>
2021-04-08 09:35:59 +0300

    more pics

--- src/data/imgData.js
+++ src/data/imgData.js
@@ -1,6 +1,9 @@
 import boyOnTractor from '../images/boyOnTractor.jpg'
 import playRoom from '../images/playRoom.jpg'
 import saloon from '../images/saloon.jpg'
+import fox from '../images/fox.jpg'
+import pinapple from '../images/pinapple.jpg'
+import kidWithBaloon from '../images/kid-with-baloons.jpg'
 
 const imgData = [
     {
@@ -66,6 +69,75 @@ const imgData = [
                 startC: [354,107],
                 endC: [426,286]
             }
+            // more tags info
+        ],
+        id: 1, //??
+    },
+    {
+        title: 'fox',
+        imageUrl: "",
+        pic: fox,
+        tagsAndPlace: [
+            {
+                tagName: 'eyes',
+                startC: [106,94],
+                endC: [142,102]
+            },
+            {
+                tagName: 'tail',
+                startC: [304,138],
+                endC: [353,300]
+            },
+            {
+                tagName: 'ears',
+                startC: [77,40],
+                endC: [152,76]
+            }
+            // more tags info
+        ],
+        id: 1, //??
+    },
+    {
+        title: 'pinapple',
+        imageUrl: "",
+        pic: pinapple,
+        tagsAndPlace: [
+            {
+                tagName: 'sunglasses',
+                startC: [150,158],
+                endC: [253,186]
+            },
+            {
+                tagName: 'green ball',
+                startC: [334,126],
+                endC: [450,267]
+            },
+            
+            // more tags info
+        ],
+        id: 1, //??
+    },
+    {
+        title: 'kid with baloon',
+        imageUrl: "",
+        pic: kidWithBaloon,
+        tagsAndPlace: [
+            {
+                tagName: 'yello baloon',
+                startC: [274,89],
+                endC: [310,138]
+            },
+            {
+                tagName: 'hat',
+                startC: [178,113],
+                endC: [222,142]
+            },
+            {
+                tagName: 'shoes',
+                startC: [192,271],
+                endC: [243,282]
+            },
+            
             // more tags info
         ],
         id: 1, //??
